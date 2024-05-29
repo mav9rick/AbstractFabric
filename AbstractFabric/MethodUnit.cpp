14b690730ee7755cfbbc9932d4b2239247336af6
mav9rick <koil-3@yandex.ru>
2024-05-29 19:52:51 +0500

    Добавлены методы для C# и Java

--- AbstractFabric/MethodUnit.cpp
+++ AbstractFabric/MethodUnit.cpp
@@ -1,13 +1,84 @@
 #include "MethodUnit.h"
 
-MethodUnit::MethodUnit( const string& name, const string& returnType, Flags flags ) :
-    m_name( name ), m_returnType( returnType ), m_flags( flags ) { }
+MethodUnit::MethodUnit( const string& name, const string& returnType, Flags flags ) :m_name( name ), m_returnType( returnType ), m_flags( flags )
+{
 
+}
 void MethodUnit::add( const shared_ptr< Unit >& unit, Flags /* flags */ )
 {
     m_body.push_back( unit );
 }
 string MethodUnit::compile( unsigned int level ) const
+{
+    string result = generateShift( level );
+    if( m_flags & STATIC )
+    {
+        result += "static ";
+    } else if( m_flags & VIRTUAL )
+    {
+        result += "virtual ";
+    }
+    result += m_returnType + " ";
+    result += m_name + "()";
+    if( m_flags & CONST )
+    {
+        result += " const";
+    }
+    result += " {\n";
+    for( const auto& b : m_body )
+    {
+        result += b->compile( level + 1 );
+    }
+    result += generateShift( level ) + "}\n";
+    return result;
+}
+
+/*-----------------------------------------------------------------------------------------------------*/
+
+JavaMethod::JavaMethod( const string& name, const string& returnType, Flags flags ) :m_name( name ), m_returnType( returnType ), m_flags( flags )
+{
+
+}
+void JavaMethod::add( const shared_ptr< Unit >& unit, Flags /* flags */ )
+{
+    m_body.push_back( unit );
+}
+string JavaMethod::compile( unsigned int level ) const
+{
+    string result = generateShift( level );
+    if( m_flags & STATIC )
+    {
+        result += "static ";
+    } else if( m_flags & VIRTUAL )
+    {
+        result += "virtual ";
+    }
+    result += m_returnType + " ";
+    result += m_name + "()";
+    if( m_flags & CONST )
+    {
+        result += " const";
+    }
+    result += " {\n";
+    for( const auto& b : m_body )
+    {
+        result += b->compile( level + 1 );
+    }
+    result += generateShift( level ) + "}\n";
+    return result;
+}
+
+/*-----------------------------------------------------------------------------------------------------*/
+
+CSharpMethod::CSharpMethod( const string& name, const string& returnType, Flags flags ) :m_name( name ), m_returnType( returnType ), m_flags( flags )
+{
+
+}
+void CSharpMethod::add( const shared_ptr< Unit >& unit, Flags /* flags */ )
+{
+    m_body.push_back( unit );
+}
+string CSharpMethod::compile( unsigned int level ) const
 {
     string result = generateShift( level );
     if( m_flags & STATIC )
--- AbstractFabric/MethodUnit.h
+++ AbstractFabric/MethodUnit.h
@@ -12,8 +12,8 @@ public:
     enum Modifier
     {
         STATIC = 1,
-        CONST = 1 << 1,
-        VIRTUAL = 1 << 2
+        CONST = 2,
+        VIRTUAL = 3
     };
 public:
     MethodUnit( const string& name, const string& returnType, Flags flags );
@@ -25,5 +25,41 @@ private:
     Flags m_flags;
     vector< shared_ptr< Unit > > m_body;
 };
-
+class JavaMethod : public Unit
+{
+public:
+    enum Modifier
+    {
+        STATIC = 1,
+        FINAL = 2
+    };
+public:
+    JavaMethod( const string& name, const string& returnType, Flags flags );
+    void add( const shared_ptr< Unit >& unit, Flags /* flags */ = 0 );
+    string compile( unsigned int level = 0 ) const;
+private:
+    string m_name;
+    string m_returnType;
+    Flags m_flags;
+    vector< shared_ptr< Unit > > m_body;
+};
+class CSharpMethod : public Unit
+{
+public:
+    enum Modifier
+    {
+        STATIC = 1,
+        OVERRIDE = 2,
+        VIRTUAL = 3
+    };
+public:
+    CSharpMethod ( const string& name, const string& returnType, Flags flags );
+    void add( const shared_ptr< Unit >& unit, Flags /* flags */ = 0 );
+    string compile( unsigned int level = 0 ) const;
+private:
+    string m_name;
+    string m_returnType;
+    Flags m_flags;
+    vector< shared_ptr< Unit > > m_body;
+};
 #endif // METHODUNIT_H
