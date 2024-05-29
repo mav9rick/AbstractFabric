1f314ead9ab924703b11a11aa8191806ddb55e01
mav9rick <koil-3@yandex.ru>
2024-05-29 22:51:35 +0500

    Добавлены PrintOperatorUnit для C++,Java,C#

--- AbstractFabric/PrintOperatorUnit.cpp
+++ AbstractFabric/PrintOperatorUnit.cpp
@@ -1,14 +1,21 @@
 #include "PrintOperatorUnit.h"
 
-PrintOperatorUnit::PrintOperatorUnit( const string& text ) : m_text( text )
-{
-
-}
-void PrintOperatorUnit::add(const shared_ptr<Unit>& /* unit */, Flags /* flags */)
-{
-
-}
 string PrintOperatorUnit::compile( unsigned int level ) const
+{
+    return generateShift( level ) + "printf( \"" + m_text + "\" );\n";
+}
+//---------------------------------------------------------------------------
+string CplusplusPrintOperator::compile( unsigned int level ) const
+{
+    return generateShift( level ) + "printf( \"" + m_text + "\" );\n";
+}
+//---------------------------------------------------------------------------
+string JavaPrintOperator::compile( unsigned int level ) const
+{
+    return generateShift( level ) + "printf( \"" + m_text + "\" );\n";
+}
+//---------------------------------------------------------------------------
+string CSharpPrintOperator::compile( unsigned int level ) const
 {
     return generateShift( level ) + "printf( \"" + m_text + "\" );\n";
 }
--- AbstractFabric/PrintOperatorUnit.h
+++ AbstractFabric/PrintOperatorUnit.h
@@ -8,8 +8,31 @@
 class PrintOperatorUnit : public Unit
 {
 public:
-    explicit PrintOperatorUnit( const string& text );
-    void add(const std::shared_ptr<Unit>& /* unit */, Flags /* flags */) override;
+    explicit PrintOperatorUnit( const string& text ){};
+    string compile( unsigned int level = 0 ) const override;
+private:
+    string m_text;
+};
+class CplusplusPrintOperator : public PrintOperatorUnit
+{
+public:
+    explicit CplusplusPrintOperator( const string& text ) : PrintOperatorUnit(text){};
+    string compile( unsigned int level = 0 ) const override;
+private:
+    string m_text;
+};
+class JavaPrintOperator : public PrintOperatorUnit
+{
+public:
+    explicit JavaPrintOperator( const string& text ): PrintOperatorUnit(text){};
+    string compile( unsigned int level = 0 ) const override;
+private:
+    string m_text;
+};
+class CSharpPrintOperator : public PrintOperatorUnit
+{
+public:
+    explicit CSharpPrintOperator( const string& text ) : PrintOperatorUnit(text){};
     string compile( unsigned int level = 0 ) const override;
 private:
     string m_text;
